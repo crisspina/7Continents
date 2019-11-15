2541735b90359031653c159ab08895931983cbb3
pizza <npwchr.work@gmail.com>
2019-11-15 17:48:31 +0700

    แก้ javascript นิดหน่อย ทำหน้า main เสร็จ

--- js/worldmap.js
+++ js/worldmap.js
@@ -1,10 +1,12 @@
 var x = document.getElementById("welcome-page-inside-box");
 var y = document.getElementById("back-btn");
+var z = document.getElementById("seccond-content");
 
 function hidebox(){
 	if (x.style.display !== "none") {
 		x.style.display = "none";
 		y.style.display = "block";
+		z.style.display = "block";
 	}
 }
 
@@ -12,5 +14,6 @@ function showbox(){
 	if (y.style.display !== "none") {
 		y.style.display = "none";
 		x.style.display = "block";
+		z.style.display = "none";
 	}
 }
